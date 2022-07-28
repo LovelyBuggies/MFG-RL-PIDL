628e1e3d1d021e8ca12801f9c6061200ac7c9267
N!no <ninomyemail@gmail.com>
2022-07-28 11:09:09 -0400

    尝试V dict与神经网络同步

--- MFG_VI.py
+++ MFG_VI.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 from value_iteration import value_iteration
+from value_iteration_ac import value_iteration_ac
 from utils import get_rho_from_u, plot_rho
 
-n_cell = 16
-n_action = 4
+n_cell = 4
+n_action = 2
 T_terminal = 2
 u_max = 1
-episode = 20
+episode = 1
 
 u = 0.5 * np.ones((n_cell, n_cell * T_terminal), dtype=np.float64)
 d = np.zeros((n_cell * T_terminal, 1), dtype=np.float64)
@@ -18,6 +19,10 @@ u_hist = list()
 for loop in range(episode):
     print(loop)
     u, V = value_iteration(rho, u_max, n_action)
+    u_ac, V_ac = value_iteration_ac(rho, u_max, n_action)
+    print(u, V)
+    print()
+    print(u_ac, V_ac)
     u_hist.append(u)
     u = np.array(u_hist).mean(axis=0)
     rho = get_rho_from_u(u, d)
--- value_iteration_ac.py
+++ value_iteration_ac.py
@@ -2,8 +2,20 @@ import numpy as np
 import torch
 from torch import nn
 
+class Critic(nn.Module):
+    def __init__(self, state_dim):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(state_dim, 8),
+            nn.Linear(8, 4),
+            nn.Linear(4, 1)
+        )
+
+    def forward(self, x):
+        return self.model(torch.from_numpy(x).float())
+
 def value_iteration_ac(rho, u_max, n_action):
-    iteration = 50
+    iteration = 1
     n_cell = rho.shape[0]
     delta_u = u_max / n_action
     u_action = np.arange(0, u_max + delta_u, delta_u)
@@ -12,6 +24,8 @@ def value_iteration_ac(rho, u_max, n_action):
     T = int(T_terminal / delta_T)
     u = dict()
     V = dict()
+    critic = Critic(2)
+    critic_optimizer = torch.optim.Adam(critic.parameters(), lr=1e-4)
     u_new = np.zeros((n_cell, T))
     V_new = np.zeros((n_cell + 1, T + 1), dtype=np.float64)
 
@@ -34,10 +48,18 @@ def value_iteration_ac(rho, u_max, n_action):
                         min_value = value
                         u[(i, t)] = speed
                         V[(i, t)] = min_value
+                        for shuo in range(50):
+                            state = np.array([i, t])
+                            print(critic(state), V[(i, t)])
+                            advantage = value - critic(state)
+                            critic_loss = advantage.pow(2)
+                            critic_optimizer.zero_grad()
+                            critic_loss.backward()
+                            critic_optimizer.step()
 
     for i in range(n_cell):
         for t in range(T):
             u_new[i, t] = u[(i * n_action, t)]
-            V_new[i, t] = V[(i * n_action, t)]
+            V_new[i, t] = critic(np.array([i * n_action, t]))
 
     return u_new, V_new
