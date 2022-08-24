874aa0d030d7841f0cc839a46ff09faef3e3953d
N!no <ninomyemail@gmail.com>
2022-08-24 15:23:44 -0400

    debug ddpg

--- value_iteration.py
+++ value_iteration.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def value_iteration(n_cell, T_terminal, rho_network, fake=False):
+def value_iteration(n_cell, T_terminal, rho, fake=False):
     iteration = 36
     delta_T = 1 / n_cell
     T = int(T_terminal / delta_T)
@@ -10,7 +10,7 @@ def value_iteration(n_cell, T_terminal, rho_network, fake=False):
     for _ in range(iteration):
         for i in range(n_cell):
             for t in range(T):
-                rho_i_t = float(rho_network(np.array([i, t])) / n_cell)
+                rho_i_t = rho[i, t]
                 u[i, t] = (V[i, t + 1] - V[i + 1, t + 1]) / delta_T + 1 - rho_i_t
                 u[i, t] = 0.5 if fake else min(max(u[i, t], 0), 1)
                 V[i, t] = delta_T * (0.5 * u[i, t] ** 2 + rho_i_t * u[i, t] - u[i, t]) + (1 - u[i, t]) * V[i, t + 1] + u[i, t] * V[i + 1, t + 1]
--- value_iteration_ddpg.py
+++ value_iteration_ddpg.py
@@ -3,11 +3,10 @@ import torch
 from torch import nn
 from utils import plot_3d
 import csv
+import copy
+from value_iteration import value_iteration
+from utils import get_rho_from_u, plot_3d
 
-
-"""
-Critic part ***********************************
-"""
 class Critic(nn.Module):
     def __init__(self, state_dim):
         super().__init__()
@@ -23,50 +22,9 @@ class Critic(nn.Module):
             nn.Linear(8, 1)
         )
 
-        def init_weights(m):
-            if isinstance(m, nn.Linear):
-                torch.nn.init.xavier_uniform(m.weight)
-                m.bias.data.fill_(0.01)
-
-        self.model.apply(init_weights)
-
     def forward(self, x):
-        return self.model(torch.from_numpy(x).float())
-
-
-def train_critic_fake(n_cell, T_terminal, V_array):
-    T = n_cell * T_terminal
-    truths = []
-    keys = []
-
-    liu = Critic(2)
-    liu_optimizer = torch.optim.Adam(liu.parameters(), lr=1e-3)
-
-    for i in range(n_cell + 1):
-        for t in range(T + 1):
-            truths.append(V_array[i, t])
-            keys.append(np.array([i, t]) / n_cell)
-
-    for _ in range(1000):
-        truths = torch.tensor(truths, requires_grad=True)
-        preds = torch.reshape(liu(np.array(keys)), (1, len(truths)))
-        loss = (truths - preds).abs().mean()
-        # print(loss)
-        liu_optimizer.zero_grad()
-        loss.backward()
-        liu_optimizer.step()
-
-    pred_V = np.zeros((n_cell + 1, T + 1))
-    for i in range(n_cell + 1):
-        for t in range(T + 1):
-            pred_V[i, t] = liu(np.array([i, t]) / n_cell)
-
-    return liu
-
-
-"""
-Actor part ***********************************
-"""
+        x = self.model(torch.from_numpy(x).float())
+        return x
 
 
 class Actor(nn.Module):
@@ -84,128 +42,129 @@ class Actor(nn.Module):
             nn.Linear(8, 1)
         )
 
-        def init_weights(m):
-            if isinstance(m, nn.Linear):
-                torch.nn.init.xavier_uniform(m.weight)
-                m.bias.data.fill_(0.01)
-
-        self.model.apply(init_weights)
-
     def forward(self, x):
-        return self.model(torch.from_numpy(x).float())
+        x = self.model(torch.from_numpy(x).float())
+        return x
 
 
-def train_ddpg(n_cell, T_terminal, rho_network, critic):
+def train_critic_fake(n_cell, T_terminal, V_array):
+    T = n_cell * T_terminal
+    truths = []
+    keys = []
+
+    liu = Critic(2)
+    liu_optimizer = torch.optim.Adam(liu.parameters(), lr=1e-3)
+
+    for i in range(n_cell + 1):
+        for t in range(T + 1):
+            truths.append(V_array[i, t])
+            keys.append(np.array([i, t]))
+
+    for _ in range(1000):
+        truths = torch.tensor(truths, requires_grad=True)
+        preds = torch.reshape(liu(np.array(keys)), (1, len(truths)))
+        loss = (truths - preds).abs().mean()
+        # print(loss)
+        liu_optimizer.zero_grad()
+        loss.backward()
+        liu_optimizer.step()
+
+    pred_V = np.zeros((n_cell + 1, T + 1))
+    for i in range(n_cell + 1):
+        for t in range(T + 1):
+            pred_V[i, t] = liu(np.array([i, t]))
+
+    return liu
+
+
+def train_ddpg(rho, d, iterations):
+    n_cell = rho.shape[0]
+    T_terminal = int(rho.shape[1] / rho.shape[0])
     delta_T = 1 / n_cell
     T = int(T_terminal / delta_T)
+    V = np.ones((n_cell + 1, T + 1))
+    states = list()
+    rhos = list()
 
     actor = Actor(2)
     actor_optimizer = torch.optim.Adam(actor.parameters(), lr=1e-3)
 
-    shuo = Critic(2)
-    shuo_optimizer = torch.optim.Adam(shuo.parameters(), lr=1e-3)
+    fake_critic = train_critic_fake(n_cell, T_terminal, value_iteration(n_cell, T_terminal, rho, fake=True)[1])
+    critic = Critic(2)
+    critic_optimizer = torch.optim.Adam(critic.parameters(), lr=1e-3)
 
-    states, truths, rhos = list(), list(), list()
+    # use value iteration to get the expected V table
     for i in range(n_cell):
         for t in range(T):
-            rho_i_t = float(rho_network(np.array([i, t]) / n_cell))
-            states.append(np.array([i, t]) / n_cell)
-            rhos.append(rho_i_t)
+            states.append([i, t])
+            rhos.append(rho[i, t])
 
     states = np.array(states)
     rhos = torch.tensor(np.reshape(np.array(rhos), (n_cell * T, 1)))
-    for _ in range(1000):
+
+    for a_it in range(iterations):
+        # train critic
+        if a_it % 50 == 0:
+            keys = list()
+            truths = list()
+            for i in range(n_cell + 1):
+                for t in range(T + 1):
+                    keys.append([i, t])
+                    speed = float(actor.forward(np.array([i, t])))
+                    if t != T:
+                        if i != n_cell:
+                            truths.append(delta_T * (0.5 * speed ** 2 + rho[i, t] * speed - speed) + fake_critic(
+                            np.array([i + speed, t + 1])))
+                        else:
+                            truths.append(delta_T * (0.5 * speed ** 2 + rho[0, t] * speed - speed) + fake_critic(
+                                np.array([speed, t + 1])))
+                    else:
+                        if i != n_cell:
+                            truths.append(delta_T * (0.5 * speed ** 2 + rho[i, t - 1] * speed - speed))
+                        else:
+                            truths.append(delta_T * (0.5 * speed ** 2 + rho[0, t - 1] * speed - speed))
+
+            truths = torch.tensor(truths, requires_grad=True)
+            for c_it in range(5000):
+                preds = torch.reshape(critic(np.array(keys)), (1, len(truths)))
+                critic_loss = (truths - preds).abs().mean()
+                print(f"{a_it, c_it} critic loss", float(critic_loss))
+                critic_optimizer.zero_grad()
+                critic_loss.backward()
+                critic_optimizer.step()
+                if critic_loss < 1e-4:
+                    break
+
+            fake_critic = critic
+
+        # train actor
         speeds = actor.forward(states)
-        next_xs = np.reshape(states[:, 0], (n_cell * T, 1)) + speeds.detach().numpy() / n_cell
-        next_ts = np.reshape(states[:, 1], (n_cell * T, 1)) + np.ones((n_cell * T, 1)) / n_cell
+        next_xs = np.reshape(states[:, 0], (n_cell * T, 1)) + speeds.detach().numpy()
+        next_ts = np.reshape(states[:, 1], (n_cell * T, 1)) + np.ones((n_cell * T, 1))
         next_states = np.append(next_xs, next_ts, axis=1)
         advantages = delta_T * (0.5 * speeds ** 2 + rhos * speeds - speeds) + critic.forward(next_states) - critic(
             states)
         policy_loss = advantages.mean()
-        # print(policy_loss)
+        if a_it % 5 == 0:
+            print(f"{a_it, c_it} policy loss", float(policy_loss))
+
         actor_optimizer.zero_grad()
         policy_loss.backward()
         actor_optimizer.step()
 
-    for i in range(n_cell):
-        for t in range(T):
-            rho_i_t = float(rho_network(np.array([i, t]) / n_cell))
-            speed = float(actor.forward(np.array([i, t]) / n_cell))
-            if t < T - 1:
-                truths.append(delta_T * (0.5 * speed ** 2 + rho_i_t * speed - speed) + critic(np.array([i + speed, t + 1]) / n_cell))
-            else:
-                truths.append(delta_T * (0.5 * speed ** 2 + rho_i_t * speed - speed))
+        if a_it % 50 == 0:
+            u_new = np.zeros((n_cell, T))
+            V_new = np.zeros((n_cell + 1, T + 1), dtype=np.float64)
+            for i in range(n_cell + 1):
+                for t in range(T + 1):
+                    if i < n_cell and t < T:
+                        u_new[i, t] = actor(np.array([i, t]))
 
-    truths = torch.tensor(truths, requires_grad=True)
-    for _ in range(1000):
-        preds = torch.reshape(shuo(np.array(states)), (1, len(truths)))
-        loss = (truths - preds).abs().mean()
-        # print(loss)
-        shuo_optimizer.zero_grad()
-        loss.backward()
-        shuo_optimizer.step()
+                    V_new[i, t] = V[i, t]
 
-    u_new = np.zeros((n_cell, T))
-    V_new = np.zeros((n_cell + 1, T + 1), dtype=np.float64)
-    for i in range(n_cell + 1):
-        for t in range(T + 1):
-            if i < n_cell and t < T:
-                u_new[i, t] = actor(np.array([i / n_cell, t / n_cell]))
-
-            V_new[i, t] = critic(np.array([i / n_cell, t / n_cell]))
-
-    return u_new, V_new, actor, shuo
+            rho = get_rho_from_u(u_new, d)
+            plot_3d(32, 1, u_new, f"./fig/u/{a_it}-{c_it}.png")
+            plot_3d(32, 1, rho,  f"./fig/rho/{a_it}-{c_it}.png")
 
 
 
-"""
-Rho part ***********************************
-"""
-class RhoNetwork(nn.Module):
-    def __init__(self, state_dim):
-        super().__init__()
-        self.model = nn.Sequential(
-            nn.Linear(state_dim, 64),
-            nn.ReLU(),
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Linear(32, 16),
-            nn.ReLU(),
-            nn.Linear(16, 8),
-            nn.ReLU(),
-            nn.Linear(8, 1)
-        )
-
-        def init_weights(m):
-            if isinstance(m, nn.Linear):
-                torch.nn.init.xavier_uniform(m.weight)
-                m.bias.data.fill_(0.01)
-
-        self.model.apply(init_weights)
-
-    def forward(self, x):
-        return self.model(torch.from_numpy(x).float())
-
-
-def train_rho(n_cell, T_terminal, rho_array):
-    T = n_cell * T_terminal
-    csv_list = []
-    truths = []
-    keys = []
-    for i in range(len(rho_array)):
-        for j in range(len(rho_array[0])):
-            truths.append(rho_array[i, j])
-            keys.append(np.array([i, j]) / n_cell)
-
-    rho = RhoNetwork(2)
-    rho_optimizer = torch.optim.Adam(rho.parameters(), lr=1e-3)
-    for _ in range(2000):
-        truths = torch.tensor(truths, requires_grad=True)
-        preds = torch.reshape(rho(np.array(keys)), (1, len(truths)))
-        loss = (truths - preds).abs().mean()
-        # print(loss)
-        rho_optimizer.zero_grad()
-        loss.backward()
-        rho_optimizer.step()
-
-    return rho
