65e4f891d2f467f1af7df217af20d29692ae129b
N!no <ninomyemail@gmail.com>
2022-09-11 21:25:39 -0400

    make supervised rho network

--- value_iteration_ddpg.py
+++ value_iteration_ddpg.py
@@ -31,41 +31,41 @@ from utils import get_rho_from_u, plot_3d
 #         return x
 
 
-# class RhoNetwork(nn.Module):
-#     def __init__(self, state_dim):
-#         super().__init__()
-#         self.model = nn.Sequential(
-#             nn.Linear(state_dim, 64),
-#             nn.ReLU(),
-#             nn.Linear(64, 32),
-#             nn.ReLU(),
-#             nn.Linear(32, 16),
-#             nn.ReLU(),
-#             nn.Linear(16, 8),
-#             nn.ReLU(),
-#             nn.Linear(8, 1)
-#         )
-#
-#     def forward(self, x):
-#         return self.model(torch.from_numpy(x).float())
+class RhoNetwork(nn.Module):
+    def __init__(self, state_dim):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(state_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 1)
+        )
+
+    def forward(self, x):
+        return self.model(torch.from_numpy(x).float())
 
 
-# def train_rho_network(n_cell, T_terminal, rho, rho_network, rho_optimizer):
-#     truths, keys = list(), list()
-#     for i in range(len(rho)):
-#         for j in range(len(rho[0])):
-#             truths.append(rho[i, j])
-#             keys.append(np.array([i, j]) / n_cell)
-#
-#     for _ in range(2000):
-#         truths = torch.tensor(truths, requires_grad=True)
-#         preds = torch.reshape(rho_network(np.array(keys)), (1, len(truths)))
-#         loss = (truths - preds).abs().mean()
-#         rho_optimizer.zero_grad()
-#         loss.backward()
-#         rho_optimizer.step()
-#
-#     return rho_network
+def train_rho_network(n_cell, T_terminal, rho, rho_network, rho_optimizer):
+    truths, keys = list(), list()
+    for i in range(len(rho)):
+        for j in range(len(rho[0])):
+            truths.append(rho[i, j])
+            keys.append(np.array([i, j]) / n_cell)
+
+    for _ in range(2000):
+        truths = torch.tensor(truths, requires_grad=True)
+        preds = torch.reshape(rho_network(np.array(keys)), (1, len(truths)))
+        loss = (truths - preds).abs().mean()
+        rho_optimizer.zero_grad()
+        loss.backward()
+        rho_optimizer.step()
+
+    return rho_network
 
 
 def train_ddpg(n_cell, T_terminal, d, iterations):
@@ -77,15 +77,15 @@ def train_ddpg(n_cell, T_terminal, d, iterations):
     V = np.zeros((n_cell + 1, T + 1), dtype=np.float64)
     rho = get_rho_from_u(u, d)
 
-    # rho_network = RhoNetwork(2)
-    # rho_optimizer = torch.optim.Adam(rho_network.parameters(), lr=1e-3)
-    # rho_network = train_rho_network(n_cell, T_terminal, rho, rho_network, rho_optimizer)
+    rho_network = RhoNetwork(2)
+    rho_optimizer = torch.optim.Adam(rho_network.parameters(), lr=1e-3)
+    rho_network = train_rho_network(n_cell, T_terminal, rho, rho_network, rho_optimizer)
 
     for it in range(iterations):
         # train V
         for t in range(T):
             for i in range(n_cell):
-                rho_i_t = rho[i, t]
+                rho_i_t = float(rho_network.forward(np.array([i, t]) / n_cell))
                 u[i, t] = (V[i, t + 1] - V[i + 1, t + 1]) / delta_T + 1 - rho_i_t
                 u[i, t] = min(max(u[i, t], 0), 1)
                 V[i, t] = delta_T * (0.5 * u[i, t] ** 2 + rho_i_t * u[i, t] - u[i, t]) + (1 - u[i, t]) * V[
@@ -96,7 +96,7 @@ def train_ddpg(n_cell, T_terminal, d, iterations):
         u_hist.append(u)
         u = np.array(u_hist).mean(axis=0)
         rho = get_rho_from_u(u, d)
-        # rho_network = train_rho_network(n_cell, T_terminal, rho, rho_network, rho_optimizer)
+        rho_network = train_rho_network(n_cell, T_terminal, rho, rho_network, rho_optimizer)
 
         if it % 10 == 0 and it != 0:
             plot_3d(32, 1, u, f"./fig/u/{it}.png")  # show without fp
