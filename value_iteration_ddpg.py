08411dd530edf425fc56513dfe25042e4f3d0ff3
N!no <ninomyemail@gmail.com>
2022-09-22 17:42:30 -0400

    make everything works!!

--- value_iteration_ddpg.py
+++ value_iteration_ddpg.py
@@ -147,20 +147,48 @@ def train_ddpg(n_cell, T_terminal, d, iterations):
     rho_network = train_rho_network_one_step(n_cell, T_terminal, rho, rho_network, rho_optimizer)
 
     for it in range(iterations):
+        # train critic
+        if it % 20 == 0:
+            keys, truths = list(), list()
+            for i in range(n_cell + 1):
+                for t in range(T + 1):
+                    keys.append([i / n_cell, t / n_cell])
+                    if t != T:
+                        if i != n_cell:
+                            speed = float(actor.forward(np.array([i, t]) / n_cell))
+                            truths.append(delta_T * (0.5 * speed ** 2 + rho[i, t] * speed - speed) + fake_critic(
+                                np.array([i + speed, t + 1]) / n_cell))
+                        else:
+                            speed = float(actor.forward(np.array([0, t]) / n_cell))
+                            truths.append(delta_T * (0.5 * speed ** 2 + rho[0, t] * speed - speed) + fake_critic(
+                                np.array([speed, t + 1]) / n_cell))
+                    else:
+                        truths.append(0)
+
+            truths = torch.tensor(truths, requires_grad=True)
+            for _ in range(100):
+                preds = torch.reshape(critic(np.array(keys)), (1, len(truths)))
+                critic_loss = (truths - preds).abs().mean()
+                critic_optimizer.zero_grad()
+                critic_loss.backward()
+                critic_optimizer.step()
+
+            fake_critic = critic
+
         states, rhos, Vs, Vus = list(), list(), list(), list()
         for i in range(n_cell):
             for t in range(T):
                 rho_i_t = float(rho_network.forward(np.array([i, t]) / n_cell))
-                speed = float(actor.forward(np.array([i / n_cell, t / n_cell])))
-                V[i, t] = delta_T * (0.5 * speed ** 2 + rho_i_t * speed - speed) + (1 - speed) * V[i, t + 1] + speed * V[i + 1, t + 1]
+                # speed = float(actor.forward(np.array([i / n_cell, t / n_cell])))
+                # V[i, t] = delta_T * (0.5 * speed ** 2 + rho_i_t * speed - speed) + (1 - speed) * V[i, t + 1] + speed * V[i + 1, t + 1]
                 states.append([i / n_cell, t / n_cell])
                 rhos.append(rho_i_t)
 
-        V[-1, :] = V[0, :].copy()
+        # V[-1, :] = V[0, :].copy()
         for i in range(T):
             for t in range(n_cell):
-                Vs.append(V[i, t + 1] - V[i, t])
-                Vus.append(V[i + 1, t + 1] - V[i, t + 1])
+                Vs.append(float(critic(np.array([i, t + 1]) / n_cell) - critic(np.array([i, t]) / n_cell)))
+                Vus.append(float(critic(np.array([i + 1, t + 1]) / n_cell) - critic(np.array([i, t + 1]) / n_cell)))
 
         states = np.array(states)
         rhos = torch.tensor(np.reshape(np.array(rhos), (n_cell * T, 1)))
@@ -173,6 +201,7 @@ def train_ddpg(n_cell, T_terminal, d, iterations):
             actor_optimizer.zero_grad()
             policy_loss.backward()
             actor_optimizer.step()
+
 
         u = np.zeros((n_cell, T))
         for i in range(n_cell):
