e80db7488001976f5d729d6e56de0f8c9a78ebd7
N!no <ninomyemail@gmail.com>
2022-09-22 18:29:23 -0400

    make non-sep work (#4)
    * remove actor
    
    * make non-sep
    
    * Update value_iteration_ddpg.py
    
    * Update MFG_VI.py
    
    * make supervised rho network
    
    * update u,V,rho network together
    
    * use actor + V + rho net
    
    * Xu's debug
    
    * Create data_rho_8.csv
    
    * use 8
    
    * actor + rho net work
    
    * actor + rho network works
    
    * make actor + rho network work
    
    * make everything works!!
    
    * prep for merge

--- MFG_VI.py
+++ MFG_VI.py
@@ -1,47 +1,11 @@
 import numpy as np
 from value_iteration_ddpg import train_ddpg
-from utils import get_rho_from_u
+import pandas as pd
 
 if __name__ == '__main__':
-    n_cell = 32
+    n_cell = 8
     T_terminal = 1
-    T = n_cell * T_terminal
-    u = 0.5 * np.ones((n_cell, T))
-    d = np.array([
-        0.39999,
-        0.39998,
-        0.39993,
-        0.39977,
-        0.39936,
-        0.39833,
-        0.39602,
-        0.39131,
-        0.38259,
-        0.36804,
-        0.34619,
-        0.31695,
-        0.28248,
-        0.24752,
-        0.21861,
-        0.20216,
-        0.20216,
-        0.21861,
-        0.24752,
-        0.28248,
-        0.31695,
-        0.34619,
-        0.36804,
-        0.38259,
-        0.39131,
-        0.39602,
-        0.39833,
-        0.39936,
-        0.39977,
-        0.39993,
-        0.39998,
-        0.39999
-    ])
-    rho = get_rho_from_u(u, d)
-    u = train_ddpg(rho, d, 100000)
-
-
+    data = pd.read_csv('data_rho_8.csv')
+    rho = np.array(data.iloc[:, 1:len(data.iloc[0, :])])
+    d = rho[:, 0]
+    u, rho = train_ddpg(n_cell, T_terminal, d, 3000)
--- value_iteration_ddpg.py
+++ value_iteration_ddpg.py
@@ -1,32 +1,7 @@
 import numpy as np
 import torch
 from torch import nn
-from utils import plot_3d
-import csv
-import copy
-from value_iteration import value_iteration
 from utils import get_rho_from_u, plot_3d
-
-class Critic(nn.Module):
-    def __init__(self, state_dim):
-        super(Critic, self).__init__()
-        self.model = nn.Sequential(
-            nn.Linear(state_dim, 64),
-            nn.ReLU(),
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Linear(32, 16),
-            nn.ReLU(),
-            nn.Linear(16, 8),
-            nn.ReLU(),
-            nn.Linear(8, 1)
-        )
-
-    def forward(self, x):
-        x = self.model(torch.from_numpy(x).float())
-        x = torch.tanh(x)
-        x = x / 2
-        return x
 
 
 class Actor(nn.Module):
@@ -55,6 +30,47 @@ class Actor(nn.Module):
         x = torch.tanh(x)
         x = (x + 1) / 2
         return x
+
+
+class Critic(nn.Module):
+    def __init__(self, state_dim):
+        super(Critic, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(state_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 1)
+        )
+
+    def forward(self, x):
+        x = self.model(torch.from_numpy(x).float())
+        x = torch.tanh(x)
+        x = x / 2
+        return x
+
+
+class RhoNetwork(nn.Module):
+    def __init__(self, state_dim):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(state_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 1)
+        )
+
+    def forward(self, x):
+        return self.model(torch.from_numpy(x).float())
 
 
 def train_critic_fake(n_cell, T_terminal, V_array):
@@ -87,13 +103,31 @@ def train_critic_fake(n_cell, T_terminal, V_array):
     return liu
 
 
-def train_ddpg(rho, d, iterations):
-    n_cell = rho.shape[0]
-    T_terminal = int(rho.shape[1] / rho.shape[0])
+def train_rho_network_one_step(n_cell, T_terminal, rho, rho_network, rho_optimizer):
+    truths, keys = list(), list()
+    for i in range(len(rho)):
+        for j in range(len(rho[0])):
+            truths.append(rho[i, j])
+            keys.append(np.array([i, j]) / n_cell)
+
+    for _ in range(1):
+        truths = torch.tensor(truths, requires_grad=True)
+        preds = torch.reshape(rho_network(np.array(keys)), (1, len(truths)))
+        loss = (truths - preds).abs().mean()
+        rho_optimizer.zero_grad()
+        loss.backward()
+        rho_optimizer.step()
+
+    return rho_network
+
+
+def train_ddpg(n_cell, T_terminal, d, iterations):
     delta_T = 1 / n_cell
     T = int(T_terminal / delta_T)
     u_hist = list()
-    rho_hist = list()
+
+    u_init = 0.5 * np.ones((n_cell, T))
+    rho = get_rho_from_u(u_init, d)
 
     actor = Actor(2)
     actor_optimizer = torch.optim.Adam(actor.parameters(), lr=1e-3)
@@ -102,82 +136,77 @@ def train_ddpg(rho, d, iterations):
     critic = Critic(2)
     critic_optimizer = torch.optim.Adam(critic.parameters(), lr=1e-3)
 
-    for a_it in range(iterations):
-        # give current rhos to calculate advantage
-        states = list()
-        rhos = list()
-        for i in range(n_cell):
-            for t in range(T):
-                states.append([i / n_cell, t / n_cell])
-                rhos.append(rho[i, t])
+    rho_network = RhoNetwork(2)
+    rho_optimizer = torch.optim.Adam(rho_network.parameters(), lr=1e-3)
+    rho_network = train_rho_network_one_step(n_cell, T_terminal, rho, rho_network, rho_optimizer)
 
-        states = np.array(states)
-        rhos = torch.tensor(np.reshape(np.array(rhos), (n_cell * T, 1)))
-
+    for it in range(iterations):
         # train critic
-        if a_it % 50 == 0:
-            keys = list()
-            truths = list()
+        if it % 1 == 0:
+            keys, truths = list(), list()
             for i in range(n_cell + 1):
                 for t in range(T + 1):
                     keys.append([i / n_cell, t / n_cell])
-                    speed = float(actor.forward(np.array([i, t]) / n_cell))
                     if t != T:
                         if i != n_cell:
+                            speed = float(actor.forward(np.array([i, t]) / n_cell))
                             truths.append(delta_T * (0.5 * speed ** 2 + rho[i, t] * speed - speed) + fake_critic(
                                 np.array([i + speed, t + 1]) / n_cell))
                         else:
+                            speed = float(actor.forward(np.array([0, t]) / n_cell))
                             truths.append(delta_T * (0.5 * speed ** 2 + rho[0, t] * speed - speed) + fake_critic(
                                 np.array([speed, t + 1]) / n_cell))
                     else:
                         truths.append(0)
 
             truths = torch.tensor(truths, requires_grad=True)
-            for c_it in range(5000):
+            for _ in range(100):
                 preds = torch.reshape(critic(np.array(keys)), (1, len(truths)))
                 critic_loss = (truths - preds).abs().mean()
-                if c_it % 100 == 0:
-                    print(f"{c_it} critic loss", float(critic_loss))
                 critic_optimizer.zero_grad()
                 critic_loss.backward()
                 critic_optimizer.step()
-                if critic_loss < 1e-4:
-                    break
 
             fake_critic = critic
 
         # train actor
-        speeds = actor.forward(states)
-        next_xs_1 = np.reshape(states[:, 0], (n_cell * T, 1))
-        next_xs_2 = np.reshape(states[:, 0], (n_cell * T, 1)) + np.ones((n_cell * T, 1)) / n_cell
-        next_xs = torch.reshape(torch.tensor(states[:, 0]), (n_cell * T, 1)) + speeds.detach().numpy() / n_cell
-        next_ts = np.reshape(states[:, 1], (n_cell * T, 1)) + np.ones((n_cell * T, 1)) / n_cell
-        next_states_1 = np.append(next_xs_1, next_ts, axis=1)
-        next_states_2 = np.append(next_xs_2, next_ts, axis=1)
-        next_states = np.append(next_xs, next_ts, axis=1)
-        interp_V_next_state = (torch.ones((n_cell * T, 1)) - speeds) * critic.forward(
-            next_states_1) + speeds * critic.forward(next_states_2)
-        advantages = delta_T * (0.5 * speeds ** 2 + rhos * speeds - speeds) + interp_V_next_state - critic(states)
-        policy_loss = advantages.mean()
-        if a_it % 5 == 0:
-            # print(max(critic.forward(next_states) - interp_V_next_state))
-            print(f"{a_it} policy loss", float(policy_loss))
-
-        actor_optimizer.zero_grad()
-        policy_loss.backward()
-        actor_optimizer.step()
-
-        u_new = np.zeros((n_cell, T))
+        states, rhos, Vs, Vus = list(), list(), list(), list()
         for i in range(n_cell):
             for t in range(T):
-                u_new[i, t] = actor(np.array([i, t]) / n_cell)
+                rho_i_t = float(rho_network.forward(np.array([i, t]) / n_cell))
+                states.append([i / n_cell, t / n_cell])
+                rhos.append(rho_i_t)
 
-        u_hist.append(u_new)
-        u_mean = np.array(u_hist[-10:]).mean(axis=0)
-        rho = get_rho_from_u(u_mean, d)
-        rho_hist.append(rho)
-        rho = np.array(rho_hist[-1:]).mean(axis=0)
-        if a_it % 200 == 0 and a_it != 0:
-            plot_3d(32, 1, u_new, f"./fig/u/{a_it}.png")  # show without fp
-            plot_3d(32, 1, np.array(rho_hist[-10:]).mean(axis=0),  f"./fig/rho_mean/{a_it}.png")  # show with fp
-            plot_3d(32, 1, rho, f"./fig/rho/{a_it}.png")  # show with fp on rho
+        for i in range(T):
+            for t in range(n_cell):
+                Vs.append(float(critic(np.array([i, t + 1]) / n_cell) - critic(np.array([i, t]) / n_cell)))
+                Vus.append(float(critic(np.array([i + 1, t + 1]) / n_cell) - critic(np.array([i, t + 1]) / n_cell)))
+
+        states = np.array(states)
+        rhos = torch.tensor(np.reshape(np.array(rhos), (n_cell * T, 1)))
+        Vs = torch.tensor(np.reshape(np.array(Vs), (n_cell * T, 1)))
+        Vus = torch.tensor(np.reshape(np.array(Vus), (n_cell * T, 1)))
+        for _ in range(1):
+            speeds = actor.forward(states)
+            advantages = delta_T * (0.5 * speeds ** 2 + rhos * speeds - speeds) + Vus * speeds + Vs
+            policy_loss = advantages.mean()
+            actor_optimizer.zero_grad()
+            policy_loss.backward()
+            actor_optimizer.step()
+
+        # train rho net
+        u = np.zeros((n_cell, T))
+        for i in range(n_cell):
+            for t in range(T):
+                u[i, t] = actor(np.array([i, t]) / n_cell)
+
+        u_hist.append(u)
+        u = np.array(u_hist).mean(axis=0)
+        rho = get_rho_from_u(u, d)
+        rho_network = train_rho_network_one_step(n_cell, T_terminal, rho, rho_network, rho_optimizer)
+
+        if it % 20 == 0 and it != 0:
+            plot_3d(n_cell, T_terminal, u, f"./fig/u/{it}.png")  # show without fp
+            plot_3d(n_cell, T_terminal, rho, f"./fig/rho/{it}.png")  # show with fp on rho
+
+    return u, rho
