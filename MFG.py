f17c27487993b971781e850b121190fd7a257d5c
N!no <ninomyemail@gmail.com>
2023-02-26 16:53:11 -0500

    update

--- MFG.py
+++ MFG.py
@@ -5,14 +5,14 @@ from utils import plot_diff
 from value_iteration_ddpg import train_ddpg
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     n_cell = 8
     T_terminal = 1
 
     algs = ["pidl", "rl+pidl"]
     options = ["lwr", "non-sep", "sep"]
     alg, option = algs[0], options[0]
-    d = np.loadtxt(f"data/rho-{option}.txt")[:, 0].flatten('F')
+    d = np.loadtxt(f"data/rho-{option}.txt")[:, 0].flatten("F")
     train_ddpg(
         alg,
         option,
